87c0ab7267dbb82efa20233e3c7651aadc98d24b
zhihong.he <omo2013@126.com>
2018-01-04 15:20:51 +0800

    init

--- infrastructure-configuration/src/main/java/cn/com/omo/infrastructure/configuration/core/GeneralLocalConfiguration.java
+++ infrastructure-configuration/src/main/java/cn/com/omo/infrastructure/configuration/core/GeneralLocalConfiguration.java
@@ -7,8 +7,10 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Service;
 
+import cn.com.omo.infrastructure.cache.Cacheable;
+import cn.com.omo.infrastructure.cache.core.ConcurrentLocalCache;
+import cn.com.omo.infrastructure.cache.core.LocalCache;
 import cn.com.omo.infrastructure.configuration.core.source.ConfigSource;
 
 /**
@@ -19,16 +21,50 @@ import cn.com.omo.infrastructure.configuration.core.source.ConfigSource;
  * @version 
  * @since 1.0
   */
-@Service("GeneralLocalConfigurationService")
-public class GeneralLocalConfiguration extends AbstractLocalConfiguration implements InitializingBean, ApplicationContextAware {
+public class GeneralLocalConfiguration extends AbstractLocalConfiguration implements Cacheable, InitializingBean, ApplicationContextAware {
 
     private ApplicationContext applicationContext;
 
+    private Map<String, ConfigSource> configSources;
+
+    private LocalCache<String, String> cache;
+
     private MultiSourceConfiguration configs;
+
+    private boolean needCache = true;
 
     @Override
     public String getStringValue(String key) {
-        return configs.getStringValue(key);
+        String bean = cache.getBean(key);
+        if (bean == null) {
+            bean = configs.getStringValue(key);
+            if (isNeedCache(bean)) {
+                cache.cacheBean(key, bean);
+            }
+        }
+
+        return bean;
+    }
+
+    @Override
+    public boolean isNeedCache(Object bean) {
+        if (bean != null && needCache) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void setCache(LocalCache<String, String> cache) {
+        this.cache = cache;
+    }
+
+    public void setConfigs(MultiSourceConfiguration configs) {
+        this.configs = configs;
+    }
+
+    public void setNeedCache(boolean needCache) {
+        this.needCache = needCache;
     }
 
     @Override
@@ -38,19 +74,40 @@ public class GeneralLocalConfiguration extends AbstractLocalConfiguration implem
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        checkApplicationContext();
+        loadConfigSources();
+        loadLocalCache();
+        loadMultiSourceConfiguration();
+    }
+
+    private void checkApplicationContext() {
         if (applicationContext == null) {
             throw new IllegalArgumentException("Spring.applicationContext must not be null");
         }
+    }
 
-        Map<String, ConfigSource> configSources = applicationContext.getBeansOfType(ConfigSource.class);
+    private void loadConfigSources() {
+        configSources = applicationContext.getBeansOfType(ConfigSource.class);
         if (configSources == null || configSources.isEmpty()) {
             throw new IllegalArgumentException("ConfigSource must not be null");
         }
+    }
 
-        configs = new GeneralMultiSourceConfiguration();
+    private void loadLocalCache() {
+        if (cache == null) {
+            cache = new ConcurrentLocalCache<String>();
+        }
+    }
+
+    private void loadMultiSourceConfiguration() {
+        if (configs == null) {
+            configs = new GeneralMultiSourceConfiguration();
+        }
+
         for (Entry<String, ConfigSource> configSource: configSources.entrySet()) {
             configs.addSource(configSource.getValue());
         }
+
         configs.sortSources();
     }
 }
